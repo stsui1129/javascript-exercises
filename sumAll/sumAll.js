ce01eac31afbb16f1f26d065e399220294d1e5ea
Sheng <s.tsui@outlook.com>
2020-08-18 20:23:59 +1200

    sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
-}
-
+const sumAll = function(first, second) {
+    if (first < 0 || second < 0) return "ERROR";
+    if (typeof first !== "number" || typeof second !== "number") return "ERROR";
+    if (first > second) {
+      const temp = first;
+      first = second;
+      second = temp;
+    }
+    let sum = 0;
+    for (let i = first; i < second + 1; i++) {
+      sum += i;
+    }
+    return sum;
+  };
+  
 module.exports = sumAll
