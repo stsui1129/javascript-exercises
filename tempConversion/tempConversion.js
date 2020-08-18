58b842e63aab3a4569982dd5f0776770ceb2c23e
Sheng <s.tsui@outlook.com>
2020-08-18 20:41:57 +1200

    tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,10 @@
-const ftoc = function() {
-
-}
-
-const ctof = function() {
-
-}
+let ftoc = function(f) {
+  return Math.round((f - 32) * (5/9) * 10) / 10;
+ }
+ 
+ let ctof = function(c) {
+   return Math.round(((c * 9/5) + 32) * 10) / 10;
+ }
 
 module.exports = {
   ftoc,
